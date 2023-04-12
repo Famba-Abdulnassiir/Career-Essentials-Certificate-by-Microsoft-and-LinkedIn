40cdb0c10b2eb43fab801126c3d8f0ae2b844d4b
Famba Abdulnassiir Siraji <57795345+FambaAbdulnassiirSiraji@users.noreply.github.com>
2023-04-12 15:15:42 +0300

    Fundamentals of Programming
    This chapter continues from where we left in the introduction Chapter and covers further more the concept/ fundamentals for any programming lang but we used python as a study case.

---
+++ Exercise Files/Chap04/04_challenge.py
@@ -0,0 +1,10 @@
+# Ask the user "What's my favorite food?"
+
+favorite_food = input ("What's my favorite food?")
+
+if favorite_food == "Posho" or "posho":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+
+print("Thanks for playing")
---
+++ Exercise Files/Chap05/05_challenge.py
@@ -0,0 +1,8 @@
+# Create a program that names your favourite cities, this should contain atleast one function.
+
+def favorite_city(name):
+    print(f"One of my favorite cities is ", name)
+
+favorite_city("Kampala")
+favorite_city("Jinja")
+favorite_city("Fortportal")
